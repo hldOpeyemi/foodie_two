2166ec3d04ca77e89b5329e2c737287da7dce84b
Opeyemi <yemi.otu@outlook.com>
2024-01-08 21:38:56 -0700

    patch - client update

--- app.py
+++ app.py
@@ -20,12 +20,15 @@ def validate_token(func):
   if (len(result) == 0):
    return make_response(jsonify("invalid token, please login"), 403)
   
-  print("Jollof Rice", result)
+  # print("Jollof Rice", result)
 
   response = func(*args, **kwargs)
 
 
+
   return response
+  #Renaming the function name:
+ wrapper.__name__ = func.__name__
  return wrapper
 
 def validate_restaurant_token(func):
@@ -50,9 +53,11 @@ def validate_restaurant_token(func):
   return response
  return restaurant_wrapper
 
-client_columns = ['id', 'first_name', 'last_name', 'image_url', 'username','password', 'email_address']
+client_columns = ['id', 'first_name', 'last_name', 'image_url', 'username', 'password', 'email_address']
 restaurant_columns = ['id', 'name', 'email_address', 'address', 'phone_number', 'bio','city', 'profile_url', 'banner_url', 'password']
 token_columns = ['client_id', 'token']
+client_signup_columns = ['id', 'username']
+
 
 @app.get("/")
 def health_check():
@@ -80,6 +85,7 @@ def client_login():
   result = run_statement('CALL delete_session(?)', [client["id"]])
 
   token_string = secrets.token_hex(16)
+
   print(token_string)
   result = run_statement('CALL post_client_login(?,?)', [client["id"], token_string])
 
@@ -104,17 +110,72 @@ def client_signup():
  try:
   first_name = request.json.get('first_name')
   last_name = request.json.get('last_name')
-  email_address = request.json.get('email address')
   image_url = request.json.get ('image_url')
   username =request.json.get ('username')
   password = request.json.get('password')
+  email_address = request.json.get('email_address')
   
-  result = run_statement('CALL client_signup(?,?,?,?,?)', [first_name, last_name, email_address, image_url, username, password])
+  result = run_statement('CALL client_signup(?,?,?,?,?,?)', [first_name, last_name, image_url, username, password, email_address])
   
-  print(result)
-  client = serialize_data(client_columns, result)[0]
+  # print(result)
+
+  client = serialize_data(client_signup_columns, result)[0]
+
+  # print(client)
 
   return make_response(jsonify(client),  200)
+ except:
+  return make_response("This is an error", 400)
+ 
+
+@app.patch("/api/client")
+@validate_token
+def client_update():
+ try:
+
+  # Creating a empty obj to hold either passed value or None for later updating
+  client_data = {}
+
+  # TO DO
+
+  client_data['first_name'] = request.json.get('first_name') if request.json.get('first_name') else None
+  client_data['last_name'] = request.json.get('last_name') if request.json.get('last_name') else None
+  client_data['email_address'] = request.json.get('email_address') if request.json.get('email_address') else None
+  client_data['image_url'] = request.json.get('image_url') if request.json.get('image_url') else None
+  client_data['username'] = request.json.get('username') if request.json.get('username') else None
+  client_data['password'] = request.json.get('password') if request.json.get('password') else None
+  
+
+  # print(client_data)
+
+  token = request.headers.get('token')
+  # print("CHECK THIS", token)
+  
+  session_columns = ['client_id', 'token']
+
+  result = run_statement('CALL get_session_by_token(?)', [token])
+  
+  session = serialize_data(session_columns, result)[0]
+
+  # print(session)
+  
+  result = run_statement(
+   'CALL client_update(?,?,?,?,?,?)', 
+   [
+    session['client_id'],
+    client_data['first_name'],
+    client_data['last_name'], 
+    client_data['email_address'],
+    client_data['image_url'],
+    client_data['username'],
+    client_data['password'] 
+  ]
+  )
+  
+  print(result)
+  # client = serialize_data(client_columns, result)[0]
+
+  return make_response(jsonify("Client succesfully updated"),  200)
  except:
   return make_response("This is an error", 400)
 
